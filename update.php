a039010d69a3e5af4fe5ad451e05195ba3c0f46a
Perzval506 <mearnsjed123@gmail.com>
2025-06-25 09:08:30 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,35 @@
-include('config/db.php');//mao ni pang connect sa file
+<?php
+include('config.php');
+
+$id = $_POST['id'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$year_graduated = $_POST['year_graduated'];
+$course_graduated = $_POST['course_graduated'];
+$gender = $_POST['gender'];
+$current_job = $_POST['current_job'];
+$current_employer = $_POST['current_employer'];
+
+$qry = "
+UPDATE alumni
+SET 
+    first_name='$first_name',
+    last_name='$last_name',
+    year_graduated='$year_graduated',
+    course_graduated='$course_graduated',
+    gender='$gender',
+    current_job='$current_job',
+    current_employer='$current_employer'
+WHERE id = '$id';
+";
+
+if ($conn->query($qry) === TRUE) {
+    echo "Update successful!";
+    header("Location: ../views/list.php");
+    exit();
+} else {
+    echo "MySQLi error: " . $conn->error;
+}
+
+$conn->close();
+?>
