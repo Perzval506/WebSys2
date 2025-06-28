cecd7526e12024e4a74cac640512c1cde30eb0f4
Brian-estanislao <brianmiguelestanislao@gmail.com>
2025-06-28 09:58:37 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,14 +2,15 @@
 include('C:\xampp\htdocs\alumni_tracker\config.php');
 
 $id = $_GET['id'];
-
 $qry = "DELETE FROM alumni WHERE id = '$id';";
 
 if ($conn->query($qry) === TRUE) {
     echo "Alumni deleted successfully!";
     header("Location: ../views/list.php");
     exit();
-} else {
+} 
+else 
+{
     echo "MySQLi error: " . $conn->error;
 }
 
